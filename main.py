b793298d068799d832ce86028bb44cbd004963d6
Samuel Fabiano <89883269+samuelfabiano@users.noreply.github.com>
2022-10-26 22:24:29 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-from combustivel import calc_combustivel
-
-if __name__ == '__main__':
-    tempo = int(input('Qual tempo percorrido (em horas):'))
-    velocidade_media = int(input('Qual velocidade média:'))
-    litros = calc_combustivel.calcula_litros(tempo,velocidade_media)
-    print("Será necessário {:.3f} litros".format(litros))
